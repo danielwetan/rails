5c250a4f65bc3cf7fdbb77e90e6ed167a717252b
Daniel Saputra <danielwetan.io@gmail.com>
2022-12-17 16:40:54 +0700

    feat: create users table

--- first-api/db/schema.rb
+++ first-api/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_151035) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_17_093248) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", limit: 200, null: false
+    t.string "full_name", limit: 255, null: false
+    t.string "email", limit: 150, null: false
+    t.string "password", limit: 255, null: false
+    t.string "role", limit: 20, default: "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
